6a0059b2387534046e9ac4d043b69c2168f28fde
chiaS <pretend15@gmail.com>
2014-11-17 16:08:41 -0800

    successfully fetched data from node server

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,19 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var exports = module.exports = {};
+
+//store message objects {"username":"Silvia","text":"message","roomname":"hello"}
+var messages = [
+{'username': 'Silvia',
+              'text' : 'message',
+              'roomname' : 'hello'
+  },
+ {'username': 'Amy',
+              'text' : 'sick',
+              'roomname' : 'home'
+  }
+];
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -39,11 +52,12 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
   response.writeHead(statusCode, headers);
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +66,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+
+  response.end(JSON.stringify(messages));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,4 +85,6 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+exports.requestHandler = requestHandler;
+console.log("modeul.exports:", module.exports);
 
